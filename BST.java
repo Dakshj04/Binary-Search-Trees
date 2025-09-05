aa2e9bf8d03b803154bed6084bed20655bb3e058
Dakshj04 <0408jaindaksh@gmail.com>
2025-09-05 15:26:37 +0530

    Bst to a Balance Bst

--- BST.java
+++ BST.java
@@ -1,4 +1,9 @@
 
+import java.util.ArrayList;
+
+
+
+
 public class BST{
     static class Node{
         int data;
@@ -29,6 +34,16 @@ public class BST{
       }
       inorder(root.left);
       System.out.print(root.data+" ");
+      inorder(root.right);
+      
+
+    }
+    public static void preorder(Node root){
+      if(root==null){
+        return;
+      }
+      System.out.print(root.data+" ");
+      inorder(root.left);
       inorder(root.right);
       
 
@@ -116,14 +131,77 @@ public class BST{
         
          return  isValidBst(root.left, min, root) && isValidBst(root.right , root, max);
       }
+
+      public static Node mirrorBst(Node root){
+        if (root==null) {
+          return root;
+        }
+       Node leftsub=mirrorBst(root.left);
+        Node rightsub=mirrorBst(root.right);
+        root.left=rightsub;
+        root.right=leftsub;
+
+        return root;
+      }
+       public static Node sortedArrayToBST(int[] nums,int start,int end) {
+        if(start>end){
+            return null;
+        }
+        int mid=(start+end)/2;
+        Node newNode=new Node(nums[mid]);
+       newNode.left= sortedArrayToBST(nums,start,mid-1);
+       newNode.right= sortedArrayToBST(nums,mid+1,end);
+        return newNode;
+         
+    }   
+   public static void getInorder(Node root,ArrayList<Integer> inorder){
+    if(root==null){
+      return;
+    }
+    getInorder(root.left, inorder);
+    inorder.add(root.data);
+    getInorder(root.right, inorder);
+   }
+
+   public static Node createBST(ArrayList<Integer> inorder,int st,int end){
+    if(st>end){
+      return null;
+    }
+     int mid=(st+end)/2;
+     Node root= new Node(inorder.get(mid));
+      createBST(inorder, st, mid-1);
+     createBST(inorder, mid+1, end);
+
+     return root;
+      
+   }
+    public static Node balanceBST(Node root){
+      //inorder sequence
+       ArrayList<Integer> inorder=new ArrayList<>();
+        getInorder(root, inorder);
+         
+
+      //sorted inorder -> balanced bst
+      return root;
+    }
   public static void main(String[] args) {
    
- int values[]={8,5,3,1,4,6,10,11,14};
- Node root=null;
+//  int values[]={8,5,3,1,4,6,10,11,14};
+//  Node root=null;
 
- for (int i = 0; i < values.length; i++) {
-    root=insert(root, values[i]);
- }
+//  for (int i = 0; i < values.length; i++) {
+//     root=insert(root, values[i]);
+//  }
+// Manually constructing the BST for demonstration
+Node root = new Node(8);
+root.left = new Node(5);
+root.left.left = new Node(3);
+root.left.left.left = new Node(1);
+root.left.left.right = new Node(4);
+root.left.right = new Node(6);
+root.right = new Node(10);
+root.right.right = new Node(11);
+root.right.right.right = new Node(12);
 
  inorder(root);
  System.out.println();
@@ -142,5 +220,11 @@ System.out.println(searchBst(root, 8));
    else{
     System.out.println("Not Valid");
    }
-
+   mirrorBst(root);
+   preorder(root);
+   System.out.println();
+   int nums[]= {-10,-3,0,5,9};
+   Node root2=sortedArrayToBST(nums,0,nums.length-1);
+   inorder(root2);
+  
 }}
